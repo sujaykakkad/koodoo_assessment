b971ff6cdb3fd5c54a2cbb3366272c9a7ba1aec3
Sujay Kakkad <sujay.kakkad@gmail.com>
2022-03-26 01:14:21 +0530

    linting libraries added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 const accountTypeChecker = (accountBalanceHistory) => {
-    let result = false;
+  let result = false;
 
-    //This will assume that the array minimum length is 3 and the object also provided in correct format
-    const balanceDifference = accountBalanceHistory[1].account.balance.amount - accountBalanceHistory[0].account.balance.amount
-    for (let i = 2; i < accountBalanceHistory.length; i++) {
-        if ((accountBalanceHistory[i].account.balance.amount - accountBalanceHistory[i - 1].account.balance.amount) !== balanceDifference) {
-            result = true;
-            break;
-        }
+  // This will assume that the array minimum length is 3
+  // and the object also provided in correct format
+  const balanceDifference = accountBalanceHistory[1].account.balance.amount
+  - accountBalanceHistory[0].account.balance.amount;
+  for (let i = 2; i < accountBalanceHistory.length; i += 1) {
+    if ((accountBalanceHistory[i].account.balance.amount
+        - accountBalanceHistory[i - 1].account.balance.amount) !== balanceDifference) {
+      result = true;
+      break;
     }
-    return result ? "A" : "B";
+  }
+  return result ? 'A' : 'B';
 };
 
 module.exports = {
-    accountTypeChecker
-}
-
+  accountTypeChecker,
+};
